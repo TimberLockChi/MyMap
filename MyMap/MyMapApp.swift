5a795f781c0ff62cc4a1b7c1b883d004c548a2ab
chitim <timchi0305@163.com>
2023-08-08 09:46:42 +0800

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Chi Tim on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
